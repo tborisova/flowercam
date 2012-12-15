5fb5be9e3fa616f0b778536c6b24240c4e3055ce
Tsvetelina Borisova <ts.borisova3@gmail.com>
2012-12-16 00:10:29 +0200

    adding code so that when filter is applied the new picture and the old one are shown to the user
    the images are saved to public/data directory of the project. This is not a good idea and I think that a script that cleans these file after a while would be good idea

--- flowercam/app/controllers/image_controller.rb
+++ flowercam/app/controllers/image_controller.rb
@@ -7,23 +7,25 @@ class ImageController < ApplicationController
 	
 	def apply_filter
 		content = params[:file_content][:content].read
+		file_ext = File.extname(params[:file_content][:content].original_filename)
 		
 		uuid = UUID.new
 		generated_uuid = uuid.generate
-		path = "/tmp" + "/" + generated_uuid
+		@name_old = generated_uuid + file_ext
+		@name_new = generated_uuid + "new" + file_ext
 		
-		Dir::mkdir(path)
+		path = Rails.root.to_s + "/public/images/" 
 		
-		file_ext = File.extname(params[:file_content][:content].original_filename)
+		@old_image_fullpath = path + generated_uuid + file_ext
 		
-		File.open(path + "/file_name" + file_ext,"w") do |f|
+		File.open(@old_image_fullpath,"w") do |f|
 			f.write(content.force_encoding("UTF-8"))
 		end 		
-		fullpath = path + "/file_name" + file_ext
-		p "Before image magick:#{Time.now}"
-		img = Magick::Image.read(fullpath).first
+		
+		@new_image_fullpath = path + @name_new
+		
+		img = Magick::Image.read(@old_image_fullpath).first
 		result = img.posterize
-		result.write(path + '/posterize.jpg')
-		p "After image magick:#{Time.now}"
+		result.write(@new_image_fullpath)
 	end
 end
