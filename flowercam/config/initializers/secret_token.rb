596263fd1d01ff8cff67e51b9b280fec99c3bd78
Tsvetelina Borisova <ts.borisova3@gmail.com>
2012-12-15 22:41:50 +0200

    adding rails app

---
+++ flowercam/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flowercam::Application.config.secret_token = '8d7ceca948de9deca172f7c01e80b769f8f659d7b780c203575a6587871e4d86b0dc4802c4f17ffc474347beb6bd7d7efd90c93337f68ebfe29f6ee11e7d1b34'
